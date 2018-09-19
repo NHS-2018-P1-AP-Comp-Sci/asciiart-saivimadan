b9c80427c6b6b9763636db9a501722abf9d24ad0
s-madans <s-madans@53S500037002.it.bsd405.org>
2018-09-19 09:00:22 -0700

    Turning in project1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,21 +1,49 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	public static void main (String[] args) {
+		System.out.println("Here's a sad jack o lantern: \n");
+		jackOLanternTop();
+		sadJackOLantern();
+		jackOLanternBottom();
+		System.out.println("Here's a happy jack o lantern: \n");
+		jackOLanternTop();
+		happyJackOLantern();
+		jackOLanternBottom();
+		System.out.println("Here's another sad jack o lantern: \n");
+		jackOLanternTop();
+		sadJackOLantern();
+		jackOLanternBottom();
+		System.out.println("Here's another happy jack o lantern: \n");
+		jackOLanternTop();
+		happyJackOLantern();
+		jackOLanternBottom();
+		System.out.println("Halloween is almost here!!!");
+		
+	}
+	public static void jackOLanternTop() {
+		System.out.println("    ___|\"\"\"|___");
+
+	
+	}
+	public static void jackOLanternBottom() {
+		System.out.println("   \\___________/");
+		System.out.println("");
+	}
+	public static void sadJackOLantern() {
+		System.out.println("  /             \\");
+		System.out.println(" /   /\\ ___ /\\   \\" );
+		System.out.println("|       \\_/       |");
+		System.out.println("|                 |");
+		System.out.println(" \\      /\\/\\     /");
+		System.out.println("  \\   /\\/\\/\\/\\  /");
+	}
+	public static void happyJackOLantern() {
+		System.out.println("  /  /\\     /\\  \\");
+		System.out.println(" /  /__\\ _ /__\\  \\" );
+		System.out.println("|       /__\\      |");
+		System.out.println("|                 |");
+		System.out.println(" \\   \\ /\\_/\\ /   /");
+		System.out.println("  \\   \\/\\_/\\/   /");
 	}
 }
+	
